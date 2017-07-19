23fa62fccf1b5970befa302ecf4d52ebd0fb3d45
Brian Jin <brian@Laurens-Air.fios-router.home>
2017-07-19 15:30:02 -0400

    Fix header navigation issue

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -3,15 +3,16 @@ import { View, Text, StyleSheet } from 'react-native';
 import { Button } from 'react-native-elements';
 
 export default class ReviewScreen extends Component {
-  static navigationOptions = {
-    title: 'Review Jobs',
-    headerRight: <Button title="Settings" onPress={() => this.props.navigation.navigate('setting')} />
+  static navigationOptions = ({ navigation }) => {
+    const { navigate } = navigation;
+    return {
+      title: 'Review Jobs',
+      headerRight: <Button title="Settings" onPress={() => navigate('setting')} />
+    };
   };
-
   render() {
     return (
       <View style={styles.container}>
-        <Button title="Settings" onPress={() => this.props.navigation.navigate('setting')} />
         <Text>ReviewScreen</Text>
         <Text>ReviewScreen</Text>
         <Text>ReviewScreen</Text>
